c9571b153494c8e32e828bbca99cf08bd9811eac
IvanLowLLH <ivanlowlh@gmail.com>
2025-12-18 15:37:34 +0800

    Update education state only when user click save button

--- src/components/EducationDetailsForm.jsx
+++ src/components/EducationDetailsForm.jsx
@@ -1,3 +1,82 @@
+import { useState } from "react";
+
+function EducationEntry({ edu, onSave, onDelete }) {
+  const [tempEdu, setTempEdu] = useState(edu);
+
+  const handleChange = (propertyName) => (event) => {
+    setTempEdu({ ...tempEdu, [propertyName]: event.target.value });
+  };
+
+  return (
+    <form className="user-form">
+      <label for="education-school">School:</label>
+      <input
+        type="text"
+        id="education-school"
+        name="education-school"
+        value={tempEdu.school}
+        onChange={handleChange("school")}
+      />
+      <label for="education-degree">Degree:</label>
+      <input
+        type="text"
+        id="education-degree"
+        name="education-degree"
+        value={tempEdu.degree}
+        onChange={handleChange("degree")}
+      />
+      <div className="start-end-dates">
+        <div className="start-date">
+          <label for="education-start">Start date:</label>
+          <input
+            type="date"
+            id="education-start"
+            name="education-start"
+            value={tempEdu.startDate}
+            onChange={handleChange("startDate")}
+          />
+        </div>
+        <div className="end-date">
+          <label for="education-end">End date:</label>
+          <input
+            type="date"
+            id="education-end"
+            name="education-end"
+            value={tempEdu.endDate}
+            onChange={handleChange("endDate")}
+          />
+        </div>
+      </div>
+      <label for="education-location">Location:</label>
+      <input
+        type="text"
+        id="education-location"
+        name="education-location"
+        value={tempEdu.location}
+        onChange={handleChange("location")}
+      />
+      <div className="card-buttons">
+        <button
+          type="button"
+          onClick={() => {
+            onDelete(edu.id);
+          }}
+        >
+          Delete
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            onSave(tempEdu);
+          }}
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}
+
 function EducationDetailsForm({ resumeInfo, setResumeInfo }) {
   const addEducation = () => {
     setResumeInfo({
@@ -16,13 +95,10 @@ function EducationDetailsForm({ resumeInfo, setResumeInfo }) {
     });
   };
 
-  const handleEducationChange = (id, propertyName) => (e) => {
-    const updatedEducation = resumeInfo.education.map((item) => {
-      if (item.id === id) {
-        return { ...item, [propertyName]: e.target.value };
-      }
-      return item;
-    });
+  const saveEducation = (updatedItem) => {
+    const updatedEducation = resumeInfo.education.map((item) =>
+      item.id === updatedItem.id ? updatedItem : item,
+    );
     setResumeInfo({ ...resumeInfo, education: updatedEducation });
   };
 
@@ -37,59 +113,12 @@ function EducationDetailsForm({ resumeInfo, setResumeInfo }) {
     <div className="form-blocks">
       <h1>Education Details</h1>
       {resumeInfo.education.map((edu) => (
-        <form key={edu.id} className="user-form">
-          <label for="education-school">School:</label>
-          <input
-            type="text"
-            id="education-school"
-            name="education-school"
-            value={edu.school}
-            onChange={handleEducationChange(edu.id, "school")}
-          />
-          <label for="education-degree">Degree:</label>
-          <input
-            type="text"
-            id="education-degree"
-            name="education-degree"
-            value={edu.degree}
-            onChange={handleEducationChange(edu.id, "degree")}
-          />
-          <div className="start-end-dates">
-            <div className="start-date">
-              <label for="education-start">Start date:</label>
-              <input
-                type="date"
-                id="education-start"
-                name="education-start"
-                value={edu.startDate}
-                onChange={handleEducationChange(edu.id, "startDate")}
-              />
-            </div>
-            <div className="end-date">
-              <label for="education-end">End date:</label>
-              <input
-                type="date"
-                id="education-end"
-                name="education-end"
-                value={edu.endDate}
-                onChange={handleEducationChange(edu.id, "endDate")}
-              />
-            </div>
-          </div>
-          <label for="education-location">Location:</label>
-          <input
-            type="text"
-            id="education-location"
-            name="education-location"
-            value={edu.location}
-            onChange={handleEducationChange(edu.id, "location")}
-          />
-          <div className="card-buttons">
-            <button onClick={() => deleteEducation(edu.id)}>Delete</button>
-            <button>Edit</button>
-            <button>Save</button>
-          </div>
-        </form>
+        <EducationEntry
+          key={edu.id}
+          edu={edu}
+          onDelete={deleteEducation}
+          onSave={saveEducation}
+        />
       ))}
       <button onClick={addEducation}>+ Add Education</button>
     </div>
